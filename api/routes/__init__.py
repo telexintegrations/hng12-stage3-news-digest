0edc20ba81622051cb8e4bf2d7b3d299bff792f6
Ayobamidele Ewetuga <ayobamideleewetuga@gmail.com>
2025-02-18 18:29:07 +0100

    fix: registered router

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from api.routes.news import news_router
+from api.routes.telex import telex_router
 
 api_version_one = APIRouter(prefix="/api/v1")
 
 api_version_one.include_router(news_router, tags=["News"])
+api_version_one.include_router(telex_router, tags=["Telex"])
